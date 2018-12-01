980550e5e4761abc7e596db96c19d5439cf21527
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2018-12-01 19:10:26 +0100

    Update userpage.js

--- public/user/userpage.js
+++ public/user/userpage.js
@@ -130,7 +130,7 @@ function searchTreesByName(){
 
   var foundTrees = [];
   trees.forEach(function(tree) {
-    if( tree.toLowerCase().contains( treeToSearch.toLowerCase() ) )
+    if( tree.name.toLowerCase().contains( treeToSearch.toLowerCase() ) )
         {
             foundTrees.push(tree.name);
         }
